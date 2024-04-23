fdb0b0c336cb1a89ba724d965383ae4de7828dbd
mattia lugaro <mattialugaro@gmail.com>
2024-04-23 19:45:32 +0200

    implemento delle foto

--- MangaVillage/Controllers/FotoController.cs
+++ MangaVillage/Controllers/FotoController.cs
@@ -18,21 +18,6 @@ namespace MangaVillage.Controllers
             return View(foto.ToList());
         }
 
-        // GET: Foto/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Foto foto = db.Foto.Find(id);
-            if (foto == null)
-            {
-                return HttpNotFound();
-            }
-            return View(foto);
-        }
-
         // GET: Foto/Create
         public ActionResult Create()
         {
@@ -54,39 +39,6 @@ namespace MangaVillage.Controllers
                 return RedirectToAction("Index");
             }
 
-            ViewBag.IDMangaFk = new SelectList(db.Manga, "ID", "Titolo", foto.IDMangaFk);
-            return View(foto);
-        }
-
-        // GET: Foto/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Foto foto = db.Foto.Find(id);
-            if (foto == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.IDMangaFk = new SelectList(db.Manga, "ID", "Titolo", foto.IDMangaFk);
-            return View(foto);
-        }
-
-        // POST: Foto/Edit/5
-        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
-        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Nome,IDMangaFk")] Foto foto)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(foto).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
             ViewBag.IDMangaFk = new SelectList(db.Manga, "ID", "Titolo", foto.IDMangaFk);
             return View(foto);
         }
